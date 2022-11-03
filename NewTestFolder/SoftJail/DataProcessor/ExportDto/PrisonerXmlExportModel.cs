e2c2b20fe28436c5b80bcd9f4c72441e84ecb802
Karolina Borisova <kkaarrii1010@gmail.com>
2022-11-03 23:06:39 +0200

    add 'SoftJail'

---
+++ NewTestFolder/SoftJail/DataProcessor/ExportDto/PrisonerXmlExportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ExportDto
+{
+    [XmlType("Prisoner")]
+   public class PrisonerXmlExportModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string IncarcerationDate { get; set; }
+
+        [XmlArray]
+        public MessageXmlModel[] EncryptedMessages { get; set; }
+    }
+}
